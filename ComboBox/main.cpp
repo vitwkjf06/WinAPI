cc920869b2787b8d3cc568468c643cf236c8c1f3
user <user@WIN-RV3D993QKOS>
2025-11-19 19:43:44 +0300

    ComboBox DONE

--- ComboBox/main.cpp
+++ ComboBox/main.cpp
@@ -1,4 +1,6 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <Windows.h>
+#include <cstdio>
 #include "resource.h"
 
 CONST CHAR* g_sz_VALUES[] = { "This","is","my","first","Combo","Box" };
@@ -29,6 +31,24 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		switch (LOWORD(wParam))
 		{
 		case IDOK:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};
+			CHAR sz_message[SIZE]{};
+			HWND hCombo = GetDlgItem(hwnd, IDC_COMBO1);
+			INT i = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
+			if (i != CB_ERR)
+			{
+				SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)sz_buffer);
+				sprintf(sz_message, "Вы выбрали элемент  N№i со значением \"%s\".", i, sz_buffer);
+				MessageBox(hwnd, sz_message, "Info", MB_OK | MB_ICONINFORMATION);
+			}
+			else
+			{
+				MessageBox(hwnd, "Вы ничего не выбрали", "Info", MB_OK | MB_ICONINFORMATION);
+			}
+
+		}
 			break;
 		case IDCANCEL:
 			EndDialog(hwnd, 0);
